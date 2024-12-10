2a31066eac4839435d8ecb6520d09eee0ed92463
serega <s.esipenko@gmail.com>
2024-12-10 22:11:27 +0700

    Hw 1 without asteriks works

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,71 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	if str == "" {
+		return "", nil
+	}
+
+	chars := []rune(str)
+
+	//gr := uniseg.NewGraphemes(str)
+
+	result := make([]rune, 0)
+	var pChar rune
+
+	//if unicode.IsDigit(chars[0]) {
+	//	return "", ErrInvalidString
+	//}
+
+	for idx, r := range chars {
+		if unicode.IsDigit(r) {
+			amount, err := strconv.Atoi(string(r))
+
+			if err != nil {
+				return "", err
+			}
+
+			if idx == 0 {
+				return "", ErrInvalidString
+			}
+
+			if unicode.IsDigit(pChar) {
+				return "", ErrInvalidString
+			}
+
+			if amount == 0 {
+				result = result[:len(result)-1]
+				continue
+			}
+
+			for range amount - 1 {
+				result = append(result, result[len(result)-1])
+			}
+		} else {
+			result = append(result, r)
+		}
+
+		pChar = r
+	}
+
+	return string(result), nil
+	// Iterate over grapheme clusters
+	//for gr.Next() {
+	//	r := gr.Runes()
+	//
+	//	if ()
+	//	if len(r) > 1 {
+	//		result = result
+	//		fmt.Print(string(r[0]), " ")
+	//
+	//	}
+	//	fmt.Println(gr.Runes())
+	//}
+	//return "", nil
 }
