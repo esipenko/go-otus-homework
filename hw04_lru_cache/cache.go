239cc5be3aff084a336748f329fda6f8439bc709
serega <s.esipenko@gmail.com>
2025-01-09 21:25:50 +0700

    HW-4: done

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,6 +1,15 @@
 package hw04lrucache
 
+import (
+	"sync"
+)
+
 type Key string
+
+type mapValue struct {
+	Key
+	value interface{}
+}
 
 type Cache interface {
 	Set(key Key, value interface{}) bool
@@ -9,17 +18,66 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
+	mx       sync.Mutex
 	items    map[Key]*ListItem
+}
+
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	defer c.mx.Unlock()
+	c.mx.Lock()
+
+	myVal, ok := c.items[key]
+
+	// Иначе не знаю как за O(1) удалить значение из мапы
+	newValue := mapValue{key, value}
+
+	if ok {
+		myVal.Value = newValue
+
+		c.queue.MoveToFront(myVal)
+		return true
+	}
+
+	newLi := c.queue.PushFront(newValue)
+	c.items[key] = newLi
+
+	if c.queue.Len() > c.capacity {
+		lastEl := c.queue.Back()
+		c.queue.Remove(lastEl)
+		delete(c.items, lastEl.Value.(mapValue).Key)
+	}
+
+	return false
+}
+
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	defer c.mx.Unlock()
+	c.mx.Lock()
+
+	val, ok := c.items[key]
+
+	if !ok {
+		return nil, false
+	}
+
+	c.queue.MoveToFront(val)
+	return val.Value.(mapValue).value, true
+}
+
+func (c *lruCache) Clear() {
+	defer c.mx.Unlock()
+	c.mx.Lock()
+
+	c.items = make(map[Key]*ListItem)
+	c.queue = NewList()
 }
 
 func NewCache(capacity int) Cache {
 	return &lruCache{
 		capacity: capacity,
 		queue:    NewList(),
-		items:    make(map[Key]*ListItem, capacity),
+		items:    make(map[Key]*ListItem),
 	}
 }
--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -6,6 +6,138 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func getListEntries(l List) []int {
+	elems := make([]int, 0, l.Len())
+
+	for i := l.Front(); i != nil; i = i.Next {
+		elems = append(elems, i.Value.(int))
+	}
+
+	return elems
+}
+
+func TestPush(t *testing.T) {
+	t.Run("push front", func(t *testing.T) {
+		l := NewList()
+		l.PushFront(10)
+		l.PushFront(20)
+
+		require.Equal(t, 2, l.Len())
+		require.Equal(t, []int{20, 10}, getListEntries(l))
+	})
+
+	t.Run("push back", func(t *testing.T) {
+		l := NewList()
+
+		l.PushBack(10)
+		l.PushBack(20)
+
+		require.Equal(t, 2, l.Len())
+
+		require.Equal(t, []int{10, 20}, getListEntries(l))
+	})
+}
+
+func TestRemove(t *testing.T) {
+	t.Run("remove from len 1", func(t *testing.T) {
+		l := NewList()
+
+		l.PushBack(10)
+		li := l.Front()
+
+		l.Remove(li)
+
+		require.Equal(t, 0, l.Len())
+		require.Nil(t, l.Back())
+		require.Nil(t, l.Front())
+	})
+
+	t.Run("remove from front", func(t *testing.T) {
+		l := NewList()
+
+		l.PushBack(10)
+		l.PushBack(20)
+
+		li := l.Front()
+
+		l.Remove(li)
+
+		require.Equal(t, 1, l.Len())
+		require.Equal(t, l.Back(), l.Front())
+	})
+
+	t.Run("remove from back", func(t *testing.T) {
+		l := NewList()
+
+		l.PushBack(10)
+		l.PushBack(20)
+
+		li := l.Back()
+
+		l.Remove(li)
+
+		require.Equal(t, 1, l.Len())
+		require.Equal(t, l.Back(), l.Front())
+	})
+
+	t.Run("remove from middle", func(t *testing.T) {
+		l := NewList()
+
+		l.PushBack(10)
+		l.PushBack(20)
+		l.PushBack(30)
+
+		li := l.Front().Next
+
+		l.Remove(li)
+
+		require.Equal(t, 2, l.Len())
+		require.Equal(t, []int{10, 30}, getListEntries(l))
+	})
+}
+
+func TestMoveFront(t *testing.T) {
+	t.Run("move front front", func(t *testing.T) {
+		l := NewList()
+
+		l.PushBack(10)
+		l.PushBack(20)
+		l.PushBack(30)
+
+		li := l.Front()
+		l.MoveToFront(li)
+
+		require.Equal(t, []int{10, 20, 30}, getListEntries(l))
+	})
+	t.Run("move front middle", func(t *testing.T) {
+		l := NewList()
+
+		l.PushBack(10)
+		l.PushBack(20)
+		l.PushBack(30)
+
+		li := l.Front().Next
+
+		l.MoveToFront(li)
+		require.Equal(t, []int{20, 10, 30}, getListEntries(l))
+	})
+
+	t.Run("move front back", func(t *testing.T) {
+		l := NewList()
+
+		l.PushBack(10)
+		l.PushBack(20)
+		l.PushBack(30)
+
+		li := l.Back()
+
+		l.MoveToFront(li)
+
+		require.Equal(t, []int{30, 10, 20}, getListEntries(l))
+		require.Equal(t, l.Back().Value, 20)
+	})
+}
+
 func TestList(t *testing.T) {
 	t.Run("empty list", func(t *testing.T) {
 		l := NewList()
@@ -42,10 +174,6 @@ func TestList(t *testing.T) {
 		l.MoveToFront(l.Front()) // [80, 60, 40, 10, 30, 50, 70]
 		l.MoveToFront(l.Back())  // [70, 80, 60, 40, 10, 30, 50]
 
-		elems := make([]int, 0, l.Len())
-		for i := l.Front(); i != nil; i = i.Next {
-			elems = append(elems, i.Value.(int))
-		}
-		require.Equal(t, []int{70, 80, 60, 40, 10, 30, 50}, elems)
+		require.Equal(t, []int{70, 80, 60, 40, 10, 30, 50}, getListEntries(l))
 	})
 }
