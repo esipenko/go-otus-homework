a068dc979a93eda13bf1830e58d7f02d583dfdf3
serega <s.esipenko@gmail.com>
2025-03-17 19:20:34 +0700

    HW-7: done

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"github.com/cheggaaa/pb/v3"
+	"io"
+	"os"
 )
 
 var (
@@ -10,6 +14,63 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	inFile, err := os.Open(fromPath)
+	defer inFile.Close()
+
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	outFile, err := os.Create(toPath)
+	defer outFile.Close()
+
+	if err != nil {
+		return err
+	}
+
+	fileInfo, err := inFile.Stat()
+
+	if err != nil {
+		return err
+	}
+
+	if !fileInfo.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+
+	fileSize := fileInfo.Size()
+
+	if fileSize < offset {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if offset > 0 {
+		_, err = inFile.Seek(offset, io.SeekStart)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	bytesToCopy := fileSize - offset
+
+	if limit > 0 && limit < bytesToCopy {
+		bytesToCopy = limit
+	}
+
+	bar := pb.Default.Start64(bytesToCopy)
+
+	defer bar.Finish()
+
+	barReader := bar.NewProxyReader(inFile)
+
+	_, err = io.CopyN(outFile, barReader, bytesToCopy)
+
+	if err != nil {
+		return err
+	}
+
 	return nil
+
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,108 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/require"
+	"os"
+	"testing"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("directories should throw unsupported error", func(t *testing.T) {
+		err := Copy("testdata/testDir", "out.txt", 0, 0)
+		require.Error(t, err)
+		require.Equal(t, ErrUnsupportedFile, err)
+	})
+
+	t.Run("directories should throw unsupported error", func(t *testing.T) {
+		err := Copy("/dev/urandom", "out.txt", 0, 0)
+		require.Error(t, err)
+		require.Equal(t, ErrUnsupportedFile, err)
+	})
+
+	t.Run("offset more than file length should throw error", func(t *testing.T) {
+		const fromPath = "testdata/simple_test.txt"
+		const toPath = "text.txt"
+		inFile, err := os.Open(fromPath)
+
+		if err != nil {
+			return
+		}
+
+		fileInfo, _ := inFile.Stat()
+		fileSize := fileInfo.Size()
+
+		err = Copy(fromPath, toPath, fileSize+1, 0)
+		require.Error(t, err)
+		require.Equal(t, ErrOffsetExceedsFileSize, err)
+		os.Remove(toPath)
+	})
+
+	t.Run("offset equal with file length should create empty file", func(t *testing.T) {
+		const fromPath = "testdata/simple_test.txt"
+		const toPath = "text.txt"
+		inFile, err := os.Open(fromPath)
+
+		if err != nil {
+			return
+		}
+
+		fileInfo, _ := inFile.Stat()
+		fileSize := fileInfo.Size()
+
+		Copy(fromPath, toPath, fileSize+1, 0)
+
+		outFile, _ := os.Open(toPath)
+		outFileInfo, _ := outFile.Stat()
+
+		require.Equal(t, outFileInfo.Size(), int64(0))
+		os.Remove(toPath)
+	})
+
+	t.Run("offset property works", func(t *testing.T) {
+		const fromPath = "testdata/simple_test.txt"
+		const toPath = "text.txt"
+
+		inFile, err := os.Open(fromPath)
+
+		if err != nil {
+			return
+		}
+
+		var offset int64 = 5
+
+		fileInfo, _ := inFile.Stat()
+		fileSize := fileInfo.Size()
+
+		Copy(fromPath, toPath, offset, 0)
+
+		outFile, _ := os.Open(toPath)
+		outFileInfo, _ := outFile.Stat()
+
+		require.Equal(t, outFileInfo.Size(), fileSize-offset)
+		os.Remove(toPath)
+	})
+
+	t.Run("limit property works", func(t *testing.T) {
+		const fromPath = "testdata/simple_test.txt"
+		const toPath = "text.txt"
+
+		inFile, err := os.Open(fromPath)
+
+		if err != nil {
+			return
+		}
+
+		var limit int64 = 5
+
+		fileInfo, _ := inFile.Stat()
+		fileSize := fileInfo.Size()
+
+		Copy(fromPath, toPath, 0, limit)
+
+		outFile, _ := os.Open(toPath)
+		outFileInfo, _ := outFile.Stat()
+
+		require.Equal(t, outFileInfo.Size(), fileSize-limit)
+		os.Remove(toPath)
+	})
 }
